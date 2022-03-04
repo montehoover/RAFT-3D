a09cf776f1ed395ae05f1f524314e0f07e2eb9a7
Monte Hoover <monte.b.hoover@gmail.com>
2022-03-04 17:03:17 -0500

    Debugging DeepDeform off-by-one on the height dimension

--- scripts/demo_2.py
+++ scripts/demo_2.py
@@ -59,11 +59,20 @@ def demo(args):
     model.eval()
     model.cuda()
 
-    # fx, fy, cx, cy = (1050.0, 1050.0, 480.0, 270.0)
-    # img1 = cv2.imread('assets/image1.png')
-    # img2 = cv2.imread('assets/image2.png')
-    # disp1 = frame_utils.read_gen('assets/disp1.pfm')
-    # disp2 = frame_utils.read_gen('assets/disp2.pfm')
+    fx, fy, cx, cy = (1050.0, 1050.0, 480.0, 270.0)
+    img1 = cv2.imread('assets/image1.png')
+    img2 = cv2.imread('assets/image2.png')
+    disp1 = frame_utils.read_gen('assets/disp1.pfm')
+    disp2 = frame_utils.read_gen('assets/disp2.pfm')
+
+    depth1 = torch.from_numpy(fx / disp1).float().cuda().unsqueeze(0)
+    depth2 = torch.from_numpy(fx / disp2).float().cuda().unsqueeze(0)
+    image1 = torch.from_numpy(img1).permute(2,0,1).float().cuda().unsqueeze(0)
+    image2 = torch.from_numpy(img2).permute(2,0,1).float().cuda().unsqueeze(0)
+    intrinsics_1 = torch.as_tensor([fx, fy, cx, cy]).cuda().unsqueeze(0)
+
+    image1_1, image2_1, depth1_1, depth2_1 = prepare_images_and_depths(image1, image2, depth1, depth2)
+
 
     fx, fy, cx, cy = (5.745410000000000537e+02, 5.775839999999999463e+02, 3.225230000000000246e+02, 2.385589999999999975e+02)
     img1 = cv2.imread('assets/deepdeform/train/seq070/color/000000.jpg')
@@ -73,23 +82,16 @@ def demo(args):
     disp1 = np.where(disp1 == 0, disp1.max(), disp1)
     disp1 = np.where(disp2 == 0, disp2.max(), disp2)
 
-    # depth1 = torch.from_numpy(fx / disp1).float().cuda().unsqueeze(0)
-    # depth2 = torch.from_numpy(fx / disp2).float().cuda().unsqueeze(0)
-    # image1 = torch.from_numpy(img1).permute(2,0,1).float().cuda().unsqueeze(0)
-    # image2 = torch.from_numpy(img2).permute(2,0,1).float().cuda().unsqueeze(0)
-    # intrinsics = torch.as_tensor([fx, fy, cx, cy]).cuda().unsqueeze(0)
-
     depth1 = torch.from_numpy(fx / disp1).float().cuda().unsqueeze(0)
     depth2 = torch.from_numpy(fx / disp2).float().cuda().unsqueeze(0)
     image1 = torch.from_numpy(img1).permute(2,0,1).float().cuda().unsqueeze(0)
     image2 = torch.from_numpy(img2).permute(2,0,1).float().cuda().unsqueeze(0)
-    intrinsics = torch.as_tensor([fx, fy, cx, cy]).cuda().unsqueeze(0)
+    intrinsics_2 = torch.as_tensor([fx, fy, cx, cy]).cuda().unsqueeze(0)
 
-    # image1, image2, depth1, depth2 = prepare_images_and_depths(image1, image2, depth1, depth2)
-    image1, image2, depth1, depth2 = prepare_images_and_depths(image1, image2, depth1, depth2)
+    image1_2, image2_2, depth1_2, depth2_2 = prepare_images_and_depths(image1, image2, depth1, depth2)
 
     # Ts = model(image1, image2, depth1, depth2, intrinsics, iters=16)
-    Ts = model(image1, image2, depth1, depth2, intrinsics, iters=16)
+    Ts = model(image1_1, image2_1, depth1_1, depth2_1, intrinsics_1, image1_2, image2_2, depth1_2, depth2_2, intrinsics_2, iters=16)
 
     
     # compute 2d and 3d from from SE3 field (Ts)
